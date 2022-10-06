4ee7c262468677c4de43075a97f14d0ed1a7c9f2
napasin163 <63030041@kmitl.ac.th>
2022-10-07 00:57:49 +0700

    test commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to my Project !</h1>
+      <button className="btn btn-primary">Test</button>   
+    </div>
+  );
+}
+
+export default App;
