7e17334c4e3a2878ce1fc216bc69d1cb011cae8e
Napasin_S <63030041@kmitl.ac.th>
2023-03-04 22:00:36 +0700

    Update auth.service.js

--- client/src/components/services/auth.service.js
+++ client/src/components/services/auth.service.js
@@ -1,58 +1,46 @@
 import axios from 'axios';
 
+
+
 const baseURL = "https://project-elw-for-robot-dev-using-mern-stack-nghi.vercel.app/api";
+//register request
+const register = (newUser) =>{
+    //POST requet on http://localhost:9999/api/register
+    return axios.post(`${baseURL}/register`,newUser)
+        .then(response => {
+            if(response){
+                return Promise.resolve(response)
+            }
+        })
+        .catch(error =>{
+            return Promise.reject(error.response)
+        })
+}
 
-const instance = axios.create({
-  baseURL: "/api",
-});
+//login request
+const login = (userCredential) =>{
+    return axios.post(`${baseURL}/login`,userCredential)
+        .then(response =>{
+            if(response.data.token){
+                localStorage.setItem("x-access-token",response.data.token)
+            }
+            return Promise.resolve(response.data)
+        })
+        .catch(error =>{
+            return Promise.reject(error.response.data)
+        })
 
-// Add a request interceptor to set the x-access-token header
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("x-access-token");
-    if (token) {
-      config.headers["x-access-token"] = token;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+}
 
-// Register request
-const register = (newUser) => {
-  return instance
-    .post(`${baseURL}/register`, newUser)
-    .then((response) => {
-      if (response) {
-        return Promise.resolve(response);
-      }
-    })
-    .catch((error) => {
-      return Promise.reject(error.response);
-    });
-};
+//logout service
+const logout = () =>{
+    localStorage.removeItem('x-access-token')
+    return {msg:"Logout Successfully..!"}
+}
 
-// Login request
-const login = (userCredential) => {
-  return instance
-    .post(`${baseURL}/login`, userCredential)
-    .then((response) => {
-      if (response.data.token) {
-        localStorage.setItem("x-access-token", response.data.token);
-      }
-      return Promise.resolve(response.data);
-    })
-    .catch((error) => {
-      return Promise.reject(error.response.data);
-    });
-};
+export{
+    register,
+    login,
+    logout  
+}
 
-// Logout service
-const logout = () => {
-  localStorage.removeItem("x-access-token");
-  return { msg: "Logout Successfully..!" };
-};
-
-export { register, login, logout };
